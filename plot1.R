7d6f3665ce5bd035176609017047c86803feb0e9
James Sanders <sanderjd@gmail.com>
2014-06-05 12:26:10 -0600

    Add first plot + code

---
+++ plot1.R
@@ -0,0 +1,20 @@
+data <- read.table(
+  "household_power_consumption.txt",
+  header=T,
+  sep=";",
+  na.strings="?",
+  stringsAsFactors=F
+)
+
+filtered <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+png("plot1.png", width = 480, height = 480)
+
+hist(
+  filtered$Global_active_power,
+  col="red",
+  xlab="Global Active Power (kilowatts)",
+  main="Global Active Power"
+) 
+
+dev.off()
